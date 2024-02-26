63216d4dc87db7831b1db7c34f9bf930ddde88c3
eugene <eremeeveugene@gmail.com>
2024-02-26 19:54:17 +0200

    QR code component (#2)

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Frontend-Mentor-WPF
+
+## QR code component
+
+https://www.frontendmentor.io/challenges/qr-code-component-iux_sIO_H
+
+<details>
+    <summary>Screenshots</summary>
+  
+### Expected result 1400x800
+![Expected result 1440x800](https://github.com/eremeeveugene/Frontend-Mentor-WPF/assets/59287893/b668e08f-3ba1-4239-96d8-b9047478d965)
+
+### Actual result 1400x800
+![Actual result 1440x800](https://github.com/eremeeveugene/Frontend-Mentor-WPF/assets/59287893/d3538758-c03d-4e50-a402-1de3757d56e9)
+
+### Expected result 2048x1080
+![Expected result 2048x1080](https://github.com/eremeeveugene/Frontend-Mentor-WPF/assets/59287893/1bde0163-61c5-4097-9302-222a36f299cd)
+
+### Actual result 2048x1080
+![Actual result 2048x1080](https://github.com/eremeeveugene/Frontend-Mentor-WPF/assets/59287893/faef7253-21fd-431d-8dfb-489846d6c712)
+
+</details>
---
+++ source/FrontendMentor.Assets/ResourceDictionaries/SharedResourceDictionary.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Windows;
+
+namespace FrontendMentor.Assets.ResourceDictionaries;
+
+/// <summary>
+///     A custom ResourceDictionary for WPF applications that optimizes resource loading and sharing.
+///     This class prevents the multiple loading of the same resource dictionaries by maintaining
+///     a static cache of loaded dictionaries. When a dictionary is requested, it is either retrieved
+///     from the cache (if already loaded) or loaded and then added to the cache for future use.
+///     This approach enhances performance and reduces memory usage by reusing dictionaries across
+///     the application.
+/// </summary>
+public class SharedResourceDictionary : ResourceDictionary
+{
+    private static readonly Dictionary<Uri, ResourceDictionary> SharedDictionaries = [];
+    private Uri? _sourceUri;
+
+    /// <summary>
+    ///     Overrides the Source property of ResourceDictionary. Setting this property checks if the
+    ///     resource dictionary specified by the given URI is already loaded. If so, the loaded dictionary
+    ///     is reused by adding it to the MergedDictionaries collection. If not, the dictionary is loaded
+    ///     and then added to both the MergedDictionaries collection and the SharedDictionaries cache.
+    /// </summary>
+    public new Uri? Source
+    {
+        get => _sourceUri;
+        set
+        {
+            _sourceUri = value;
+
+            if (value == null) return;
+
+            if (SharedDictionaries.TryGetValue(value, out var dictionary))
+            {
+                MergedDictionaries.Add(dictionary);
+            }
+            else
+            {
+                base.Source = value;
+
+                SharedDictionaries.Add(value, this);
+            }
+        }
+    }
+}
