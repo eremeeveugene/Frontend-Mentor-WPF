7cec3da1352f79c91292b8ee89ed8b2f3a031cc1
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-02-27 21:15:49 +0200

    Setup social links profile project

---
+++ source/FrontendMentor.SocialLinksProfile/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using FrontendMentor.SocialLinksProfile.Views;
+using Prism.Ioc;
+using System.Windows;
+
+namespace FrontendMentor.SocialLinksProfile;
+
+public partial class App
+{
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+    }
+
+    protected override Window CreateShell()
+    {
+        return Container.Resolve<SocialLinksProfileWindowView>();
+    }
+}
