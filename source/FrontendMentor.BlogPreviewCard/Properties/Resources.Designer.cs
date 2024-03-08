4d060eb93b31f517d2fb7be86f2b06ad03bb1082
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-03-08 19:29:57 +0200

    ui updates

--- source/FrontendMentor.BlogPreviewCard/Properties/Resources.Designer.cs
+++ source/FrontendMentor.BlogPreviewCard/Properties/Resources.Designer.cs
@@ -61,11 +61,56 @@ namespace FrontendMentor.BlogPreviewCard.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Greg Hooper.
+        /// </summary>
+        public static string BlogAuthor {
+            get {
+                return ResourceManager.GetString("BlogAuthor", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Learning.
+        /// </summary>
+        public static string BlogCategory {
+            get {
+                return ResourceManager.GetString("BlogCategory", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to These languages are the backbone of every website, defining structure, content, and presentation..
+        /// </summary>
+        public static string BlogDescription {
+            get {
+                return ResourceManager.GetString("BlogDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Blog Preview Card.
         /// </summary>
         public static string BlogPreviewCardWindowTitle {
             get {
                 return ResourceManager.GetString("BlogPreviewCardWindowTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Published 21 Dec 2023.
+        /// </summary>
+        public static string BlogPublishedDate {
+            get {
+                return ResourceManager.GetString("BlogPublishedDate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to HTML &amp; CSS foundations.
+        /// </summary>
+        public static string BlogTitle {
+            get {
+                return ResourceManager.GetString("BlogTitle", resourceCulture);
             }
         }
     }
