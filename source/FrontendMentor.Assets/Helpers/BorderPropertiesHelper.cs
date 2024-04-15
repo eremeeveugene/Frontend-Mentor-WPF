a94240e2f54cf522247aeb525b9dff23c90e09cb
ｅｕｇｅｎｅ <eremeeveugene@gmail.com>
2024-04-15 22:41:15 +0300

    Add button styles

---
+++ source/FrontendMentor.Assets/Helpers/BorderPropertiesHelper.cs
@@ -0,0 +1,31 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Windows;
+
+namespace FrontendMentor.Assets.Helpers;
+
+public static class BorderPropertiesHelper
+{
+    public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.RegisterAttached(
+        "CornerRadius", typeof(CornerRadius), typeof(BorderPropertiesHelper),
+        new PropertyMetadata(default(CornerRadius)));
+
+    public static void SetCornerRadius(DependencyObject element, CornerRadius value)
+    {
+        element.SetValue(CornerRadiusProperty, value);
+    }
+
+    public static CornerRadius GetCornerRadius(DependencyObject element)
+    {
+        return (CornerRadius)element.GetValue(CornerRadiusProperty);
+    }
+}
