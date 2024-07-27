fa8cc3d9cec60864e207707c9c2857853b1258a9
eugene <eremeeveugene@gmail.com>
2024-07-27 20:15:35 +0300

    ui updates

---
+++ source/FrontendMentor.Assets/Helpers/UIElementPropertiesHelper.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Windows;
+
+namespace FrontendMentor.Assets.Helpers;
+
+public static class UIElementPropertiesHelper
+{
+    public static readonly DependencyProperty IsMouseOverParentProperty = DependencyProperty.RegisterAttached(
+        "IsMouseOverParent", typeof(bool), typeof(UIElementPropertiesHelper), new PropertyMetadata(default(bool)));
+
+    public static readonly DependencyProperty IsFocusedParentProperty = DependencyProperty.RegisterAttached(
+        "IsFocusedParent", typeof(bool), typeof(UIElementPropertiesHelper), new PropertyMetadata(default(bool)));
+
+    public static void SetIsMouseOverParent(DependencyObject element, bool value)
+    {
+        element.SetValue(IsMouseOverParentProperty, value);
+    }
+
+    public static bool GetIsMouseOverParent(DependencyObject element)
+    {
+        return (bool)element.GetValue(IsMouseOverParentProperty);
+    }
+
+    public static void SetIsFocusedParent(DependencyObject element, bool value)
+    {
+        element.SetValue(IsFocusedParentProperty, value);
+    }
+
+    public static bool GetIsFocusedParent(DependencyObject element)
+    {
+        return (bool)element.GetValue(IsFocusedParentProperty);
+    }
+}
