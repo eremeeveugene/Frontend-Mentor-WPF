e86ce444a3ef0ceeb5538d786e1a947bc5cca465
eugene <eremeeveugene@gmail.com>
2024-07-27 14:20:32 +0300

    Update layout

--- source/FrontendMentor.Assets/Helpers/WindowPropertiesHelper.cs
+++ source/FrontendMentor.Assets/Helpers/WindowPropertiesHelper.cs
@@ -19,6 +19,10 @@ public static class WindowPropertiesHelper
         "IsActive", typeof(bool), typeof(WindowPropertiesHelper),
         new PropertyMetadata(default(bool)));
 
+
+    public static readonly DependencyProperty WindowStateProperty = DependencyProperty.RegisterAttached(
+        "WindowState", typeof(WindowState), typeof(WindowPropertiesHelper), new PropertyMetadata(default(WindowState)));
+
     public static void SetIsActive(DependencyObject element, bool value)
     {
         element.SetValue(IsActiveProperty, value);
@@ -27,5 +31,15 @@ public static class WindowPropertiesHelper
     public static bool GetIsActive(DependencyObject element)
     {
         return (bool)element.GetValue(IsActiveProperty);
+    }
+
+    public static void SetWindowState(DependencyObject element, WindowState value)
+    {
+        element.SetValue(WindowStateProperty, value);
+    }
+
+    public static WindowState GetWindowState(DependencyObject element)
+    {
+        return (WindowState)element.GetValue(WindowStateProperty);
     }
 }
