fc6e30ed5ca8a0ea47af65802645fa81cf7f0ec5
eugene <eremeeveugene@gmail.com>
2024-09-18 17:28:27 +0300

    Setup project

---
+++ source/Apps/FrontendMentor.ThreeColumnPreviewCardComponent/Controls/Windows/ThreeColumnPreviewCardComponentWindow.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using FrontendMentor.Assets.Controls.Windows;
+using System.Windows;
+
+namespace FrontendMentor.ThreeColumnPreviewCardComponent.Controls.Windows;
+
+internal class ThreeColumnPreviewCardComponentWindow : FrontendMentorWindow
+{
+    static ThreeColumnPreviewCardComponentWindow()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(ThreeColumnPreviewCardComponentWindow),
+            new FrameworkPropertyMetadata(typeof(ThreeColumnPreviewCardComponentWindow)));
+    }
+}
