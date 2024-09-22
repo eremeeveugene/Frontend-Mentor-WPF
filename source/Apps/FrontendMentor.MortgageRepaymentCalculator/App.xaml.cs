912a8483e05529a126eeb1470aae58769df410d3
eugene <eremeeveugene@gmail.com>
2024-09-22 21:05:03 +0300

    setup project

---
+++ source/Apps/FrontendMentor.MortgageRepaymentCalculator/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using FrontendMentor.MortgageRepaymentCalculator.Constants;
+using FrontendMentor.MortgageRepaymentCalculator.Controls.Windows;
+using FrontendMentor.MortgageRepaymentCalculator.Views;
+using System.Windows;
+
+namespace FrontendMentor.MortgageRepaymentCalculator;
+
+internal partial class App
+{
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        base.RegisterTypes(containerRegistry);
+
+        containerRegistry.RegisterForNavigation<MortgageRepaymentCalculatorView>(MortgageRepaymentCalculatorViewNames
+            .MortgageRepaymentCalculator);
+    }
+
+    protected override Window CreateShell()
+    {
+        return Container.Resolve<MortgageRepaymentCalculatorWindow>();
+    }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        NavigateToShellRegion(MortgageRepaymentCalculatorViewNames.MortgageRepaymentCalculator);
+    }
+}
