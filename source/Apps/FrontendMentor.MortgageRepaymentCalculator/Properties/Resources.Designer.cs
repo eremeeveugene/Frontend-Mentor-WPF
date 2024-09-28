3dd5b41eb00d706b40cd351482940b11862ed72f
eugene <eremeeveugene@gmail.com>
2024-09-28 21:08:35 +0300

    Add resources

--- source/Apps/FrontendMentor.MortgageRepaymentCalculator/Properties/Resources.Designer.cs
+++ source/Apps/FrontendMentor.MortgageRepaymentCalculator/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace FrontendMentor.MortgageRepaymentCalculator.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace FrontendMentor.MortgageRepaymentCalculator.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FrontendMentor.MortgageRepaymentCalculator.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace FrontendMentor.MortgageRepaymentCalculator.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -61,9 +61,18 @@ namespace FrontendMentor.MortgageRepaymentCalculator.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Mortgage Repayment Calculator.
+        /// </summary>
+        public static string MortgageRepaymentCalculatorWindow_Title {
+            get {
+                return ResourceManager.GetString("MortgageRepaymentCalculatorWindow_Title", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Interest Only.
         /// </summary>
-        internal static string MortgageType_InterestOnly {
+        public static string MortgageType_InterestOnly {
             get {
                 return ResourceManager.GetString("MortgageType_InterestOnly", resourceCulture);
             }
@@ -72,7 +81,7 @@ namespace FrontendMentor.MortgageRepaymentCalculator.Properties {
         /// <summary>
         ///   Looks up a localized string similar to Repayment.
         /// </summary>
-        internal static string MortgageType_Repayment {
+        public static string MortgageType_Repayment {
             get {
                 return ResourceManager.GetString("MortgageType_Repayment", resourceCulture);
             }
