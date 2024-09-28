8231feb7e835f9b1940e5625f3423cf9e847710c
eugene <eremeeveugene@gmail.com>
2024-09-28 21:01:21 +0300

    Add CurrencySymbols

---
+++ source/FrontendMentor.Core/Constants/CurrencySymbols.cs
@@ -0,0 +1,73 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2024 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+namespace FrontendMentor.Core.Constants;
+
+/// <summary>
+///     Provides a set of common currency symbols.
+/// </summary>
+public static class CurrencySymbols
+{
+    /// <summary>
+    ///     Represents the US Dollar symbol: $.
+    /// </summary>
+    public const string Dollar = "$";
+
+    /// <summary>
+    ///     Represents the Euro symbol: €.
+    /// </summary>
+    public const string Euro = "€";
+
+    /// <summary>
+    ///     Represents the British Pound symbol: £.
+    /// </summary>
+    public const string Pound = "£";
+
+    /// <summary>
+    ///     Represents the Japanese Yen symbol: ¥.
+    /// </summary>
+    public const string Yen = "¥";
+
+    /// <summary>
+    ///     Represents the Indian Rupee symbol: ₹.
+    /// </summary>
+    public const string Rupee = "₹";
+
+    /// <summary>
+    ///     Represents the Chinese Yuan symbol: ¥ (Same as Yen).
+    /// </summary>
+    public const string Yuan = "¥";
+
+    /// <summary>
+    ///     Represents the Swiss Franc symbol: CHF.
+    /// </summary>
+    public const string Franc = "CHF";
+
+    /// <summary>
+    ///     Represents the Russian Ruble symbol: ₽.
+    /// </summary>
+    public const string Ruble = "₽";
+
+    /// <summary>
+    ///     Represents the South Korean Won symbol: ₩.
+    /// </summary>
+    public const string Won = "₩";
+
+    /// <summary>
+    ///     Represents the Canadian Dollar symbol: CA$.
+    /// </summary>
+    public const string CanadianDollar = "CA$";
+
+    /// <summary>
+    ///     Represents the Australian Dollar symbol: A$.
+    /// </summary>
+    public const string AustralianDollar = "A$";
+}
