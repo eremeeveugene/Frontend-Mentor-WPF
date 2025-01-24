ab2b835a6faef012ead121216e82106afd6356bc
eugene <eremeeveugene@gmail.com>
2025-01-24 17:54:08 +0200

    cleanup solution

--- source/Apps/FrontendMentor.OrderSummaryComponent/App.xaml.cs
+++ source/Apps/FrontendMentor.OrderSummaryComponent/App.xaml.cs
@@ -1,12 +1,21 @@
-﻿using System.Configuration;
-using System.Data;
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
 using System.Windows;
 
 namespace FrontendMentor.OrderSummaryComponent;
+
 /// <summary>
-/// Interaction logic for App.xaml
+///     Interaction logic for App.xaml
 /// </summary>
 public partial class App : Application
 {
 }
-
