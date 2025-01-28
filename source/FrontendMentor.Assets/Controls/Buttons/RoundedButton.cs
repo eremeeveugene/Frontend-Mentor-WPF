05e6f944dfbb9e3aa089163715022c2d62e553f5
eugene <eremeeveugene@gmail.com>
2025-01-28 17:04:20 +0200

    add styles

---
+++ source/FrontendMentor.Assets/Controls/Buttons/RoundedButton.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FrontendMentor.Assets.Controls.Buttons;
+
+public class RoundedButton : Button
+{
+    public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.Register(
+        nameof(CornerRadius), typeof(CornerRadius), typeof(RoundedButton), new PropertyMetadata(default(CornerRadius)));
+
+    static RoundedButton()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(RoundedButton),
+            new FrameworkPropertyMetadata(typeof(RoundedButton)));
+    }
+
+    public CornerRadius CornerRadius
+    {
+        get => (CornerRadius)GetValue(CornerRadiusProperty);
+        set => SetValue(CornerRadiusProperty, value);
+    }
+}
