d9598c4261d70f0c55e0da62b0b746190852229c
eugene <eremeeveugene@gmail.com>
2025-01-30 19:01:51 +0200

    Setup application

---
+++ source/Apps/FrontendMentor.GitHubUserSearchApp/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using FrontendMentor.GitHubUserSearchApp.Constants;
+using FrontendMentor.GitHubUserSearchApp.Controls.Windows;
+using FrontendMentor.GitHubUserSearchApp.Views;
+using System.Windows;
+
+namespace FrontendMentor.GitHubUserSearchApp;
+
+internal partial class App
+{
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        base.RegisterTypes(containerRegistry);
+
+        //containerRegistry.RegisterSingleton<IAnnualPlanService, AnnualPlanService>();
+        containerRegistry.RegisterForNavigation<GitHubUserSearchAppView>(GitHubUserSearchAppViewNames
+            .GitHubUserSearchApp);
+    }
+
+    protected override Window CreateShell()
+    {
+        return Container.Resolve<GitHubUserSearchAppWindow>();
+    }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+
+        NavigateToShellRegion(GitHubUserSearchAppViewNames.GitHubUserSearchApp);
+    }
+}
---
+++ source/Apps/FrontendMentor.GitHubUserSearchApp/Controls/Windows/GitHubUserSearchAppWindow.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2025 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using FrontendMentor.Assets.Controls.Windows;
+using System.Windows;
+
+namespace FrontendMentor.GitHubUserSearchApp.Controls.Windows;
+
+internal class GitHubUserSearchAppWindow : FrontendMentorWindow
+{
+    static GitHubUserSearchAppWindow()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(GitHubUserSearchAppWindow),
+            new FrameworkPropertyMetadata(typeof(GitHubUserSearchAppWindow)));
+    }
+}
