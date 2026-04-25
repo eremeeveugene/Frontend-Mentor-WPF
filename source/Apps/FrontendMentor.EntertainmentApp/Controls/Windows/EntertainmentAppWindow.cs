144f77e7eca5826abad9bebc2f61d0b059d9bbd5
Eugene <eremeeveugene@gmail.com>
2026-04-25 18:14:49 +0300

    Create EntertainmentAppWindow.cs

---
+++ source/Apps/FrontendMentor.EntertainmentApp/Controls/Windows/EntertainmentAppWindow.cs
@@ -0,0 +1,25 @@
+﻿// --------------------------------------------------------------------------------
+// Copyright (C) 2026 Eugene Eremeev (also known as Yevhenii Yeriemeieiv).
+// All Rights Reserved.
+// --------------------------------------------------------------------------------
+// This software is the confidential and proprietary information of Eugene Eremeev
+// (also known as Yevhenii Yeriemeieiv) ("Confidential Information"). You shall not
+// disclose such Confidential Information and shall use it only in accordance with
+// the terms of the license agreement you entered into with Eugene Eremeev (also
+// known as Yevhenii Yeriemeieiv).
+// --------------------------------------------------------------------------------
+
+using FrontendMentor.Assets.Controls.Windows;
+using System.Windows;
+
+namespace FrontendMentor.EntertainmentApp.Controls.Windows;
+
+internal class EntertainmentAppWindow : FrontendMentorWindow
+{
+    static EntertainmentAppWindow()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(
+            typeof(EntertainmentAppWindow),
+            new FrameworkPropertyMetadata(typeof(EntertainmentAppWindow)));
+    }
+}
