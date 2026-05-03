215c038abbb2ed3b6a065413b5f86d6677687047
Eugene <eremeeveugene@gmail.com>
2026-05-03 16:06:25 +0300

    Update App.xaml.cs

--- source/Apps/FrontendMentor.EntertainmentApp/App.xaml.cs
+++ source/Apps/FrontendMentor.EntertainmentApp/App.xaml.cs
@@ -11,6 +11,7 @@
 
 using FrontendMentor.EntertainmentApp.Interfaces.ViewModels;
 using FrontendMentor.EntertainmentApp.Interfaces.Views;
+using FrontendMentor.EntertainmentApp.Services.ImageCache;
 using FrontendMentor.EntertainmentApp.Services.Tmdb;
 using FrontendMentor.EntertainmentApp.ViewModels;
 using FrontendMentor.EntertainmentApp.ViewModels.TabItems;
@@ -45,6 +46,7 @@ internal sealed partial class App
         serviceCollection.AddTransient<ISeriesTabItemView, SeriesTabItemView>();
         serviceCollection.AddTransient<ISeriesTabItemViewModel, SeriesTabItemViewModel>();
         serviceCollection.AddSingleton<ITmdbService, TmdbService>();
+        serviceCollection.AddSingleton<IImageCacheService, ImageCacheService>();
 
         var configurationBuilder = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory)
             .AddJsonFile("appsettings.json", false).Build();
